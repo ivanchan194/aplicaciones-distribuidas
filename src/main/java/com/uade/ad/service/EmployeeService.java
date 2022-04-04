fca58a4b090cb156756e2cfe9027c8d568aa8654
Ivan Chan <ivanchan.194@gmail.com>
2022-04-04 00:56:34 -0300

    test

--- src/main/java/com/uade/ad/service/EmployeeService.java
+++ src/main/java/com/uade/ad/service/EmployeeService.java
@@ -1,2 +1,17 @@
-package com.uade.ad.service;public interface EmployeeService {
+package com.uade.ad.service;
+
+import java.util.List;
+
+import com.uade.ad.model.Employee;
+
+public interface EmployeeService {
+    public List<Employee> retrieveEmployees();
+
+    public Employee getEmployee(Long employeeId);
+
+    public void saveEmployee(Employee employee);
+
+    public void deleteEmployee(Long employeeId);
+
+    public void updateEmployee(Employee employee);
 }
