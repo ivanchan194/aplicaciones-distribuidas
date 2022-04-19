10e42a9bcf6a925557de954f47193145e8307eca
Ivan Chan <ivanchan.194@gmail.com>
2022-04-19 17:12:38 -0300

    WIP

--- src/main/java/com/uade/ad/controller/EmployeeRestController.java
+++ src/main/java/com/uade/ad/controller/EmployeeRestController.java
@@ -3,8 +3,6 @@ package com.uade.ad.controller;
 import java.util.List;
 
 import com.uade.ad.model.Type;
-import com.uade.ad.model.User;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
